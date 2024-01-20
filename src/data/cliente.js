ad284ecaf8926175675be16280635fa1cddfcb6c
FelipeOvidio <f.costaovidio@gmail.com>
2024-01-19 22:26:35 -0300

    feat: criação da tabela cliente

--- src/data/cliente.js
+++ src/data/cliente.js
@@ -1,2 +1,22 @@
 const Sequelize = require('sequelize')
 const database = require('../config/db')
+
+const Cliente = database.define('cliente', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    numero: {
+        type: Sequelize.STRING(12),
+        allowNull: false
+    }
+})
+
+module.exports = Cliente
