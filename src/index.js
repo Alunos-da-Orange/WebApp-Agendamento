793c87225bdde9e8b33a6fcf6c8646d4c47a3a0c
mlsfront <mlsfront@outlook.com>
2024-01-20 13:02:14 -0300

    deleted: deletando o diretorio src/ no momento não tem utilidade src/config/db.js src/data/cliente.js src/index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-app.listen(process.env.PORT, () => console.log('Server Start'))
